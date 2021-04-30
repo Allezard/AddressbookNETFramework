9842af9c9639d3d0fcd0dc7dac3b7188ca07a1d3
Семён Елютин <allezard00@gmail.com>
2021-04-30 22:35:41 +0300

    ContactDataGeneration и GroupDataGeneration объеденины в один проект DataGeneration + мелкие правки по добавлениям комментариев.

---
+++ DataGeneration/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using AddressbookNETFramework.Model;
+
+namespace DataGeneration
+{
+    class Program
+    {
+        /// <summary>
+        /// Генерирует файл в зависимости от выбора формата(json или xml) и записывает в него значения на основе выбранных полей из "GroupData" или "ContactData".
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string type = args[0]; // Принимает 1-ый аргумент в виде названия типа ("typeGroup" или "typeContact")
+            int count = Convert.ToInt32(args[1]); // Принимает 2-ой аргумент, создает нужное кол-во файлов
+            StreamWriter writer = new StreamWriter(args[2]); // Принимает 3-ий аргумент в виде названия файла
+            string format = args[3]; // Принимает 4-ый аргумент в виде типа файла
+
+            if (type == "typeGroup")
+            {
+                List<GroupData> groups = new List<GroupData>();
+                for (int i = 0; i < count; i++)
+                {
+                    groups.Add(new GroupData()
+                    {
+                        GroupName = BaseClass.GenerateRandomString(10),
+                        GroupHeader = BaseClass.GenerateRandomString(10),
+                        GroupFooter = BaseClass.GenerateRandomString(10)
+                    });
+                }
+                if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXMLFile(groups, writer);
+                }
+                else
+                {
+                    Console.Out.WriteLine("Unrecognized format" + format);
+                }
+                writer.Close();
+            }
+            else if (type == "typeContact")
+            {
+                List<ContactData> contacts = new List<ContactData>();
+                for (int i = 0; i < count; i++)
+                {
+                    contacts.Add(new ContactData()
+                    {
+                        FirstName = BaseClass.GenerateRandomString(10),
+                        MiddleName = BaseClass.GenerateRandomString(10),
+                        LastName = BaseClass.GenerateRandomString(10),
+                        NickName = BaseClass.GenerateRandomString(10),
+                        Company = BaseClass.GenerateRandomString(10),
+                        Title = BaseClass.GenerateRandomString(10),
+                        Address = BaseClass.GenerateRandomString(10),
+                        HomePhone = BaseClass.GenerateRandomString(10),
+                        MobilePhone = BaseClass.GenerateRandomString(10),
+                        WorkPhone = BaseClass.GenerateRandomString(10),
+                        Fax = BaseClass.GenerateRandomString(10),
+                        Email = BaseClass.GenerateRandomString(10),
+                        Email2 = BaseClass.GenerateRandomString(10),
+                        Email3 = BaseClass.GenerateRandomString(10),
+                        Homepage = BaseClass.GenerateRandomString(10),
+                        SecondaryAddress = BaseClass.GenerateRandomString(10),
+                        HomeAddress = BaseClass.GenerateRandomString(10),
+                        Notes = BaseClass.GenerateRandomString(10)
+                    });
+                }
+                if (format == "json")
+                {
+                    WriteContactsToJsonFile(contacts, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteContactsToXMLFile(contacts, writer);
+                }
+                else
+                {
+                    Console.Out.WriteLine("Unrecognized format" + format);
+                }
+                writer.Close();
+            }
+            else
+            {
+                Console.Out.WriteLine("Format not selected or specified incorrectly");
+            }
+            writer.Close();
+        }
+
+        static void WriteGroupsToXMLFile(List<GroupData> groups, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
+        }
+
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        static void WriteContactsToXMLFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            new XmlSerializer(typeof(List<ContactData>)).Serialize(writer, contacts);
+        }
+
+        static void WriteContactsToJsonFile(List<ContactData> contacts, StreamWriter writer)
+        {
+            writer.Write(JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+}
--- DataGeneration/Properties/AssemblyInfo.cs
+++ DataGeneration/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("GroupDataGeneration")]
+[assembly: AssemblyTitle("DataGeneration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GroupDataGeneration")]
+[assembly: AssemblyProduct("DataGeneration")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("42fa2959-0a26-471e-bf02-9adba1cfcc74")]
+[assembly: Guid("5ad1ce86-efca-45ca-b869-95bdbb25b62a")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
